bb58fc65bd37deb72d89245d65efb22bef1be6e6
Sourav-Naik <developer.sourav.naik@gmail.com>
2024-04-06 14:31:06 +0530

    changes index

--- DataBase.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-const ConnectToMongo = async () => {
-  await mongoose.connect("mongodb://localhost:27017/enotebook");
-};
-export default ConnectToMongo;
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-import ConnectToMongo from "./DataBase.js";
 import express from "express";
-import auth from "./routes/auth.js";
-import notes from "./routes/notes.js";
+import mongoose from "mongoose";
 import cors from "cors";
 import path from "path";
-
-ConnectToMongo();
-
-const __dirname = process.cwd();
+import auth from "./routes/auth.js";
+import notes from "./routes/notes.js";
 
 const app = express();
 app.use(express.json());
@@ -24,14 +20,22 @@ app.use("/api/auth", auth);
 app.use("/api/notes", notes);
 
 // Serve static files from the "build" directory
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(process.cwd(), "build")));
 // Serve the React app for all other routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(process.cwd(), "build", "index.html"));
 });
 
 // Start the server
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+mongoose
+  .connect("mongodb://localhost:27017/enotebook")
+  .then(() => {
+    console.log("Connected to the database");
+    // Start the server
+    app.listen(5000, async () => {
+      console.log(`Server is running on port ${5000}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database: ${err}`);
+  });
